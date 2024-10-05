7ff4ffbb4dfde321b2606b3d33299cd626c6c694
PavelGor1 <116425791+PavelGor1@users.noreply.github.com>
2024-10-05 13:53:00 +0700

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,10 +1,14 @@
+x1 = int(input())
+x2 = int(input())
 a1 = int(input())
-b1 = int(input())
 a2 = int(input())
-b2 = int(input())
-if b1<a2 :
+if x1<a1<x2 and x2<a2:
+    print(a1,x2)
+elif a1<x1 and x2>a2:
+    print(x1,a2)
+elif a1 == x2:
+    print(a1)
+elif a2 == x1:
+    print(x1)
+elif x2<a1 or a2<x1:
     print("Пустое множество")
-elif b1>a2:
-    print(a2,b1)
-elif b1==a2:
-    print(b1)
